b609bcc7b73131857b44c733c8778fd439e989ef
Daniel Restrepo <jdrestrepo@unitecnica.net>
2022-07-19 19:52:30 -0500

    Adding validations to user inputs

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,6 +1,6 @@
 const {response} = require('express');
 const bcryptjs = require('bcryptjs');
-const { validationResult } = require('express-validator');
+
 
 const User = require('../models/user.model');
 
@@ -28,11 +28,7 @@ const usersPut = (req, res = response) => {
 
 const usersPost = async(req, res = response) => {
 
-    const errors = validationResult(req);
-    if( !errors.isEmpty() ){
-        return res.status(400).json(errors);
-    }
-
+    
     const { name, email, password, role} =  req.body
     const user = new User( {name, email, password, role} );
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,13 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
+const Role = require('../models/role.model');
+
 const { usersGet, 
         usersPut, 
         usersPost, 
         usersDelete } = require('../controllers/users.controller');
+const { validateInputs } = require('../middlewares/validate-inputs');
 
 const router = Router();
 
@@ -13,7 +16,16 @@ router.get('/', usersGet );
 router.put('/:id', usersPut );
 
 router.post('/',[
-        check('email', 'Email is not valid').isEmail()
+        check('name', 'Name is required').not().isEmpty(),
+        check('password', 'Password should contain six or more charaters').isLength({ min:6 }),
+        check('email', 'Email is not valid').isEmail(),
+        check('role').custom( async(role = '') => {
+                const roleExists = await Role.findOne({role});
+                if( !roleExists){
+                        throw new Error(`Role ${ role } is not registered in DB`)
+                }
+        }),
+        validateInputs
 ], usersPost );
 
 router.delete('/', usersDelete );
