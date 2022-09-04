eb2a59b10b4396531232f6af2c835b8ad7823cba
Daniel Restrepo <jdrestrepo@unitecnica.net>
2022-09-04 14:10:57 -0500

    Working on generating jwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,46 @@
 const { response } = require('express');
+const bcryptjs = require('bcryptjs');
 
-const login = (req, res = response) => {
+const userModel = require('../models/user.model');
+const { generateJWT } = require('../helpers/generateJWT.JS');
 
-    res.json({
-        msg: 'Login ok'
-    })
+const login = async(req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try{
+        //Comprobar si el usuario existe
+        const user = await userModel.findOne({email});
+        console.log(user)
+        if(!user){
+            return res.status(400).json({
+                msg: "User or password are incorrect"
+            })
+        }
+
+        //Verificar la contraseña
+        const validPasssword = bcryptjs.compareSync( password, user.password);
+        if( !validPasssword ){
+            return res.status(400).json({
+                msg: "User or password are incorrect"
+            })
+        }
+
+        //Generar el JWT
+        const token = await generateJWT(user.id)
+
+        res.json({
+            user,
+            jwt: token
+        })
+    }catch (err){
+        console.log(err);
+        res.status(500).json({
+            msg: 'Talk to admin'
+        })
+    }
+
+   
 }
 
 module.exports = {
---
+++ helpers/generateJWT.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const generateJWT = ( uid = '' ) => {
+    return new Promise( (resolve, reject) => {
+
+        const payload = { uid };
+
+        jwt.sign( payload, process.env.SECRETORPRIVATEKEY, {
+            expiresIn: '4h'
+        }, (err, token) => {
+            if(err) {
+                console.log(err);
+                reject(' Json web token could not be generate');
+            } else{
+                resolve( token )
+            }
+        } )
+    })
+}
+
+module.exports = {
+    generateJWT
+}
